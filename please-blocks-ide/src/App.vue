cf938d4c701be1ff3d089c758377c458661f87bd
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2026-06-22 23:13:54 +0700

    feat: check server status on component mount

--- please-blocks-ide/src/App.vue
+++ please-blocks-ide/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { onMounted }  from 'vue'
 import AppShell    from '@/components/layout/AppShell.vue'
 import ProjectGate from '@/components/layout/ProjectGate.vue'
 import { useRunnerStore } from '@/model/stores/runnerStore.js'
 
 const runner = useRunnerStore()
+
+onMounted(() => runner.checkServer())
 </script>
 
 <template>
